4797545fc5a791d0b019a1262292b44480862d7b
shaiknazaruddin <148045851+shaiknazaruddin@users.noreply.github.com>
2024-08-27 15:48:59 +0530

    Add files via upload

---
+++ Even number = 0 to 10;.js
@@ -0,0 +1,14 @@
+﻿let number = 1;
+
+
+for (let i=0; i<=10; i++) { 
+  if (number % 2 === 0) 
+    { 
+        console.log(number);
+    }
+    number++;
+}
+
+
+o/p :- 
+          0  2  4  6  8  10
